1dc562c97951e957e5b5dc813524d6c136da4601
Chikage <chikage@atx.tokyo>
2025-01-25 13:37:01 +0900

    fix: カバレッジ設定更新

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,17 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     include: ['src/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov', 'clover'],
+      reportsDirectory: './coverage',
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        '**/node_modules/**',
+        '**/*.test.ts',
+        'webpack.config.js'
+      ]
+    }
   },
 }); 
